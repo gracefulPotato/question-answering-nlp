dc38f2a35a9033f5d26d77eed9b26d90904d74ad
gracefulPotato <grace.ohair.sherman@gmail.com>
2018-05-17 18:14:32 -0700

    Recall up to 0.3548

--- qa.py
+++ qa.py
@@ -3,7 +3,21 @@ from qa_engine.base import QABase
 from qa_engine.score_answers import main as score_answers
 import re
 
-
+def get_keyword(question,dep_q):
+    print(question)
+    print("WHERE")
+    print(' '.join(question.split()[3:]))
+    
+    keywords = question.split()
+    #need to remove verb
+    for node in dep_q.nodes:
+        if "VB" in dep_q.nodes[node]["tag"]:
+            if node-1 in range(len(keywords)):
+                keywords[node-1] = "[^\.]*"
+                keyword = " ".join(keywords[2:])
+                keyword = re.sub("\?","",keyword)
+    return keyword
+                
 def get_answer(question, story):
     """
     :param question: dict
@@ -19,7 +33,7 @@ def get_answer(question, story):
         difficulty -- easy, medium, or hard
         type -- whether you need to use the 'sch' or 'story' versions
                 of the .
-        id  --  The id of the question.
+        qid  --  The id of the question.
 
 
     story is a dictionary with keys:
@@ -39,22 +53,24 @@ def get_answer(question, story):
     """
     text_q = question["text"]
     dep_q = question["dep"]
+    print(question["qid"])
     print(text_q)
-    #print(dep_q)
-    #for node in dep_q.nodes:
-    #    print(dep_q.nodes[node]["rel"])
+    print(story["text"])
+    print(dep_q)
     
     keyword = ""
     for node in dep_q.nodes:
         if dep_q.nodes[node]["rel"]=="nsubj":
             #print(dep_q.nodes[node]["lemma"])
-            keyword = dep_q.nodes[node]["lemma"].lower()
+            keyword = dep_q.nodes[node]["lemma"] #.lower()
     numwords = len(text_q.split(" "))
+    if keyword=="":
+        for node in dep_q.nodes:
+            if dep_q.nodes[node]["rel"]=="nobj":
+                keyword = dep_q.nodes[node]["lemma"] #.lower()
     if keyword=="":
         print("try: "+str(dep_q.nodes[numwords]["lemma"]))
         keyword = dep_q.nodes[numwords]["lemma"]
-    #matches = re.findall(keyword+"[^\.]*",story["text"])
-    #print(matches)
 
 ###############################################
     
@@ -73,32 +89,26 @@ def get_answer(question, story):
 
         if question_type_who:
             print(question_type_who.group())
-            keyword = question_type_who.group()[2:]
+            keyword = get_keyword(question_type_who.group(),dep_q)
+            #question_type_who.group()[2:]
         if question_type_what:
             print(question_type_what.group())
+            #keyword = get_keyword(question_type_what.group(),dep_q)
         if question_type_where:
             print(question_type_where.group())
             print("WHERE")
             print(' '.join(question_type_where.group().split()[3:]))
-            
-            keywords = question_type_where.group().split()
-            #need to remove verb
-            for node in dep_q.nodes:
-                if "VB" in dep_q.nodes[node]["tag"]:
-                    if node-1 in range(len(keywords)):
-                        keywords[node-1] = "[^\.]*"
-            keyword = " ".join(keywords[2:])
-            keyword = re.sub("\?","",keyword)
+            keyword = get_keyword(question_type_where.group(),dep_q)
             print(keyword)
         if question_type_when:
             print(question_type_when.group())
+            #keyword = get_keyword(question_type_when.group(),dep_q)
         if question_type_why:
             print(question_type_why.group())
-            
-        matches = re.findall(keyword+"[^\.]*",story["text"])
+            #keyword = get_keyword(question_type_why.group(),dep_q)
+        print("matching keyword: "+keyword)
+        matches = re.findall("[^\.]*"+keyword+"[^\.]*",story["text"])
         print(matches)
-                
-
 
     answer = "whatever you think the answer is"
 
@@ -106,7 +116,6 @@ def get_answer(question, story):
         return matches[0]
     else:
         return answer
-
 
 
 #############################################################
